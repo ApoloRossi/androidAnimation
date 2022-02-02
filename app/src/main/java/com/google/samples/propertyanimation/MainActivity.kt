eaa307ca1098d204680b1e6ab5c33421b49b646b
Apolo <apolorossi@gmail.com>
2022-02-01 23:36:26 -0300

    feat: Implementado animator no Fader, Colorizer e no Shower.

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -16,19 +16,19 @@
 
 package com.google.samples.propertyanimation
 
-import android.animation.Animator
-import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
-import android.animation.ValueAnimator
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.*
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.animation.addListener
-import androidx.core.animation.addPauseListener
-import androidx.core.animation.doOnEnd
-import androidx.core.animation.doOnStart
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 
 
 class MainActivity : AppCompatActivity() {
@@ -74,7 +74,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         showerButton.setOnClickListener {
-            shower()
+            multipleShower()
         }
     }
 
@@ -109,12 +109,99 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(fadeButton)
+        animator.start()
     }
 
+    @SuppressLint("ObjectAnimatorBinding")
     private fun colorizer() {
+        var animator = ObjectAnimator.ofArgb(
+            star.parent, "backgroundColor", Color.BLACK, Color.RED
+        )
+        animator.duration = 500
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(colorizeButton)
+        animator.start()
+    }
+
+    fun multipleShower() {
+        for (x in 0 .. 3) {
+            shower()
+        }
     }
 
     private fun shower() {
+        val container = star.parent as ViewGroup
+        val containerW = container.width
+        val containerH = container.height
+
+        var starW = star.width.toFloat()
+        var starH = star.height.toFloat()
+
+        /**
+         * Create the star in an ImageView component
+         */
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        /**
+         * Set a randon size for X and Y size of the star in a range of 1.0 to 1.6
+         */
+        newStar.scaleX = Math.random().toFloat() * 0.6f + 1f
+        newStar.scaleY = newStar.scaleX
+        starW *= newStar.scaleX
+        starH *= newStar.scaleY
+
+        newStar.translationX = Math.random().toFloat() * containerW - (starW / 2)
+
+        /**
+         *Set Animation to move star in axis Y from 0 - starH to container height + starH
+         *Set Interpolator to be the falling star velocity growth rate
+         */
+        val mover = ObjectAnimator.ofFloat(
+            newStar, View.TRANSLATION_Y,
+            -starH, containerH + starH
+        )
+        mover.interpolator = AccelerateInterpolator(1f)
+
+        /**
+         *Set Animation to rotate star around it self in the max of 3 times.
+         *Set Interpolator to be the rotation star velocity growth rate
+         */
+        val rotator = ObjectAnimator.ofFloat(
+            newStar, View.ROTATION,
+            (Math.random() * 1080).toFloat()
+        )
+        rotator.interpolator = LinearInterpolator()
+
+        //Add the star to the parent layout
+        container.addView(newStar)
+
+        /**
+         * AnimatorSet is used to run both animations at the same time.
+         */
+        val set = AnimatorSet()
+        set.playTogether(mover, rotator)
+        set.duration = (Math.random() * 1500 + 500).toLong()
+
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                container.removeView(newStar)
+            }
+        })
+
+        set.start()
+
+
+
     }
 
     private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
